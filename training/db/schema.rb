137d2b9b82aa6cba92adc0ceeef6d8041acdcc99
Yegor Dubinker <egoralexdubinker@googlemail.com>
2013-08-21 12:16:45 +0200

    v2.1

--- training/db/schema.rb
+++ training/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130821073948) do
+ActiveRecord::Schema.define(version: 20130820123414) do
 
   create_table "exercise_trainiglists", id: false, force: true do |t|
     t.integer  "exercise_id"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20130821073948) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "muskelgruppe"
   end
 
   create_table "exercises_traininglists", id: false, force: true do |t|
