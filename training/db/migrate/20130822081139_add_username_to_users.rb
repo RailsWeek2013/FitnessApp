8ab7f4b0c22d2b4761f007fe1d043258a95c4df4
Adnan Aktas <adnan.aktas@mni.thm.de>
2013-08-22 14:29:46 +0200

    Username gelöscht

--- training/db/migrate/20130822081139_add_username_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddUsernameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :username, :string
-  end
-
-  add_index :users, :username,                :unique => true
-end
