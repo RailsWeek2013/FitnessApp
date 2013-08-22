15f69d3e65d5c31541b8b2b71e8bd1e2a67888e2
Adnan Aktas <adnan.aktas@mni.thm.de>
2013-08-22 11:12:34 +0200

    App Core Features funktionieren alle

--- training/app/models/user.rb
+++ training/app/models/user.rb
@@ -2,10 +2,27 @@ class User < ActiveRecord::Base
 	has_many :traininglists
 	has_many :exercises
 	has_one :user_datum
+
+  #before_create :test
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   # attr_accessible :name, :email, :password, :password_confirmation, :episode_ids
+  #attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  #attr_accessor :login
+  #attr_accessible :login
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable #:authentication_keys => [:login]
+
+   #validates :username,
+  #:uniqueness => {
+    #:case_sensitive => false
+  #}
+
+  #def test
+   # name=[]
+   # 10.times do name << ('a'..'z').to_a.shuffle[0] end
+    #self.username = name.join
+  #end
 end
